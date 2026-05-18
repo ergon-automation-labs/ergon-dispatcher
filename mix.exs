4be0dd40372a20981a0cadffdbdc0c028b6daf30
Abby Malson <august.malson@gmail.com>
2026-05-18 12:46:35 -0600

    chore: bump version for test isolation fix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyDispatcher.MixProject do
   def project do
     [
       app: :bot_army_dispatcher,
-      version: "0.4.17",
+      version: "0.4.18",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
