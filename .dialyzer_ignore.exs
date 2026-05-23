828e326550c34fe405726ba63f281eec7356d1f4
Abby Malson <august.malson@gmail.com>
2026-05-23 07:48:47 -0600

    fix: ignore dialyzer unknown_function for shared MigrationRunner

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -4,5 +4,6 @@
   {"lib/bot_army_dispatcher/intent_evaluator.ex", :pattern_match},
   {"lib/bot_army_dispatcher/intent_evaluator.ex", :no_return},
   {"lib/bot_army_dispatcher/intent_evaluator.ex", :unused_fun},
-  {"lib/bot_army_dispatcher/optimization_scheduler.ex", :pattern_match}
+  {"lib/bot_army_dispatcher/optimization_scheduler.ex", :pattern_match},
+  {"lib/bot_army_dispatcher/release.ex", :unknown_function}
 ]
