963931ea41e398e4db3a4eedfcce15523ac6f3b7
Abby Malson <august.malson@gmail.com>
2026-06-16 16:53:52 -0600

    fix: Use DateTime.utc_now() instead of DateTime.now!("UTC") to avoid timezone database requirement

--- lib/bot_army_dispatcher/stores/learning_report_generator.ex
+++ lib/bot_army_dispatcher/stores/learning_report_generator.ex
@@ -14,7 +14,7 @@ defmodule BotArmyDispatcher.Stores.LearningReportGenerator do
   def generate_daily_report do
     Logger.info("[LearningReportGenerator] Generating daily learning report")
 
-    today = DateTime.now!("UTC") |> DateTime.to_date()
+    today = DateTime.utc_now() |> DateTime.to_date()
     {:ok, today_start} = DateTime.new(today, ~T[00:00:00], "UTC")
     today_end = DateTime.add(today_start, 24 * 3600, :second)
 
