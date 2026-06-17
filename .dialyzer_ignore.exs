2604a45fc38c81304d7c73b6d77824559ec91948
Abby Malson <august.malson@gmail.com>
2026-06-16 19:24:34 -0600

    Revert "chore: Add dialyzer suppressions for BotArmyCore.IntegrationGates unknown_function warnings"
    This reverts commit 6746bb86bd24cffa838b19a25f9f49da3293f9e5.

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -7,13 +7,7 @@
   {"lib/bot_army_dispatcher/optimization_scheduler.ex", :pattern_match},
   {"lib/bot_army_dispatcher/release.ex", :unknown_function},
   {"lib/bot_army_dispatcher/handlers/self_heal_handler.ex", :call_to_missing},
-  {"lib/bot_army_dispatcher/handlers/self_heal_handler.ex", :unknown_function},
   {"lib/bot_army_dispatcher/handlers/agent_dispatch_handler.ex", :call_to_missing},
-  {"lib/bot_army_dispatcher/handlers/agent_dispatch_handler.ex", :unknown_function},
   {"lib/bot_army_dispatcher/nats/consumer.ex", :call_to_missing},
-  {"lib/bot_army_dispatcher/optimization_scheduler.ex", :call_to_missing},
-  {"lib/bot_army_dispatcher/optimization_scheduler.ex", :unknown_function},
-  {"lib/bot_army_dispatcher/daily_briefing_orchestrator.ex", :unknown_function},
-  {"lib/bot_army_dispatcher/discord_publisher.ex", :unknown_function},
-  {"lib/bot_army_dispatcher/nats/briefing_responder.ex", :unknown_function}
+  {"lib/bot_army_dispatcher/optimization_scheduler.ex", :call_to_missing}
 ]
