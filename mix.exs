654142c41d80b6c1b5f67992e39c1a0c1b6b1981
Abby Malson <august.malson@gmail.com>
2026-09-04 10:59:16 +0300

    chore: pick up circuit breaker telemetry from runtime

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyDispatcher.MixProject do
   def project do
     [
       app: :bot_army_dispatcher,
-      version: "0.5.88",
+      version: "0.5.89",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
